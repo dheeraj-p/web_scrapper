f7936fc2367b45238c37f7f25ce656afe85640e3
Dheeraj Pundir <dheeraj-p@users.noreply.github.com>
2024-07-29 12:48:51 +0530

    Implement basic auth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from scrapper import ProductsScrapper
 from storage import LocalStorage, ImageDownloader
 from notifier import ConsoleNotifier
@@ -6,10 +8,22 @@ from requests import api
 
 
 app = FastAPI()
+security = HTTPBearer()
+static_token = "Bearer some_random_token"
+
+
+def get_user(creds: Annotated[HTTPAuthorizationCredentials, Depends(security)]):
+    if creds.credentials == static_token:
+        return "valid_username"
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+    )
 
 
 @app.get("/scrap")
-def scrap_pages(page_count: int = 1):
+def scrap_pages(username: Annotated[str, Depends(get_user)], page_count: int = 1):
     # Hard coded for now, can be taken from environment variables
     storage = LocalStorage("products.json")
     notifier = ConsoleNotifier()
